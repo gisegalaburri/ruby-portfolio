c3f8154be3fe3dab4597e41a384a3323a30a0341
Gisele Galaburri <gisele.galaburri89@gmail.com>
2020-12-30 00:53:17 -0300

    Implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -33,4 +33,27 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def edit
+    # it will find the portfolio by id in the db and render in the view
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  # PATCH/PUT /portfolios/1
+  # PATCH/PUT /portfolios/1.json
+  def update
+    # need to call the find
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
