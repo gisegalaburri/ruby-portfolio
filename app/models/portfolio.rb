8c84b78a9eb40d6691b3dc44bc6fc67585a4161e
Gisele Galaburri <gisele.galaburri89@gmail.com>
2021-01-16 14:57:17 -0300

    Impemented image default for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,12 +1,25 @@
 class Portfolio < ApplicationRecord
-    validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body, :main_image, :thumb_image
 
-    # custom scope
-    # self. indicates is a class method
-    def self.angular
-        where(subtitle: 'Angular')
-    end
+  # custom scope
+  # self. indicates is a class method
+  def self.angular
+      where(subtitle: 'Angular')
+  end
 
-    # another custom scope
-    scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+  # another custom scope
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+
+  # callback
+  after_initialize :set_defaults
+
+  def set_defaults
+    # ||= if the main_image is nil, if it is, set the default image
+    # shortcut of: 
+    # if main_image == nill 
+        # self.main_image ||= "https://via.placeholder.com/600x400" 
+    # end
+    self.main_image ||= "https://via.placeholder.com/600x400"
+    self.thumb_image ||= "https://via.placeholder.com/350x200"
+  end
 end
